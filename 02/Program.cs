dcaf930acc79ac005b2fb18c4adcbd9348d7df2b
Dan <baitursonov@gmail.com>
2022-11-18 01:52:07 +0600

    Lesson 01

---
+++ 02/Program.cs
@@ -0,0 +1,36 @@
+﻿//2. С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+
+/*
+//полное условие
+if ()
+{
+    //true
+}
+else
+{
+    //false
+}
+//не полное условие
+if ()
+{
+    //true
+}
+else
+{
+    //false
+} 
+*/
+
+System.Console.WriteLine("Введите число a:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+
+if(a*a==b) 
+{
+    System.Console.WriteLine($"{b} является корнем {a}");
+}
+if(b*b==a) 
+{
+    System.Console.WriteLine($"{a} является корнем {b}");
+}
