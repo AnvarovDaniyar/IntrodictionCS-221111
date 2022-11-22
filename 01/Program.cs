033da2f7884212e12fe75838b7500f8d66abccc7
Dan <baitursonov@gmail.com>
2022-11-23 01:13:48 +0600

    --

--- 01/Program.cs
+++ 01/Program.cs
@@ -5,4 +5,5 @@ System.Console.Write("Введите число:");
 string? s=Console.ReadLine();
 a=Convert.ToInt32(s);
 int b=a*a;
-System.Console.WriteLine($"{a}+^2={b}");
+System.Console.WriteLine($"{a}+^2={b}"
+);
