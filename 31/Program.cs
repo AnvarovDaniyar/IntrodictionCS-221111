28b4d77924b9c724b7ece93c9c93d31a9514e419
Dan <baitursonov@gmail.com>
2022-11-28 21:59:17 +0600

    Lesson 3

---
+++ 31/Program.cs
@@ -0,0 +1,10 @@
+﻿//31. Сгенерировать 10 случайных чисел. Показать кубы тех чисел, которые заканчиваются на четную цифру
+
+Random random=new Random();
+for(int i=0;i<10;i++)
+{
+    int a=random.Next(1,100);
+    int b=(int)Math.Pow(a,3);
+    if (b%10%2==0)
+    System.Console.WriteLine($"{a},  {b}");
+}
