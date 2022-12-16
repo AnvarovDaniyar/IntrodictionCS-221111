2c60475543a01e7a2a04e5d109e63accdf5f4eb4
Dan <baitursonov@gmail.com>
2022-12-16 22:24:21 +0600

    Home work 5

---
+++ 51/Program.cs
@@ -0,0 +1,37 @@
+﻿//51. С клавиатуры вводится число N. Затем вводятся N чисел. 
+//Определить сколько чисел больше 0 введено с клавиатуры.
+
+System.Console.WriteLine("Enter a number N:");
+int N=Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Enter {N} numbers");
+
+int[] NewNumbers(int N)
+{
+    int[] a=new int[N];
+           for(int i=0;i<N;i++)
+        a[i]=Convert.ToInt32(Console.ReadLine());
+                return a;
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+int[] b= NewNumbers(N);
+Print(b);
+
+int PositivCounter(int[] b)
+{
+       int count=0;
+    foreach(int el in b)
+    {
+        if (el>0)
+            count++;       
+    }
+    return count;
+}
+
+System.Console.WriteLine($"\n{PositivCounter(b)}");
---
+++ 56/Program.cs
@@ -0,0 +1,30 @@
+﻿//56. Написать программу копирования массива.
+
+int[] RandomIntArray(int size,int min,int max)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+int[] ArrayCopy(int[] a)
+{
+    int[] c=new int[a.Length];
+           for(int i=0;i<a.Length;i++)
+            c[i]=a[i];
+    return c;
+}
+int[] a= RandomIntArray(10,0,10);
+int[] c= ArrayCopy(a);
+System.Console.WriteLine();
+Print(a);
+Print(c);
+
+
+
