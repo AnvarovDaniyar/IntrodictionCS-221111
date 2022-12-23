3bf5da6f63ea4377f635313974e6c2118b7b009a
Dan <baitursonov@gmail.com>
2022-12-23 22:42:38 +0600

    Home work 7

---
+++ 70/Program.cs
@@ -0,0 +1,13 @@
+﻿// 70. Показать натуральные числа от 1 до N, N задано
+void Natur(int N,int i=1)
+{
+    if (i<=N)
+    {
+        System.Console.Write($"{i} ");
+        Natur(N,i+1);
+    }    
+}
+
+
+Natur(6);
+
---
+++ 71/Program.cs
@@ -0,0 +1,13 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+void Rutan(int N,int i=1)
+{
+    if (i<=N)
+    {
+        System.Console.Write($"{N} ");
+        Rutan(N-1,i);
+    }    
+}
+
+
+Rutan(10);
+
