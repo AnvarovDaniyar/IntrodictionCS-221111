d37bd832a23d4006eacdc5fd9a671259f0580b1f
Dan <baitursonov@gmail.com>
2022-12-27 23:52:33 +0600

    Home work 8

--- 84/Program.cs
+++ 84/Program.cs
@@ -1,12 +1,20 @@
 ﻿//84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. 
 // Вычислить сумму цифр введенного числа
+ string s=Console.ReadLine();
+bool Inttest(string s)
+{
+    foreach(char c in s)
+    if (char.IsLetter(c)) return false;
+    return true;
+}
+System.Console.WriteLine(Inttest(s));
 
-
-string s=Console.ReadLine();
-int k=0;
- for(int i=0;i<s.Length;i++)
-     if (char.IsDigit(s[i])) 
-        k+=s[i];
-        System.Console.WriteLine(k);
-    // else 
-    // System.Console.WriteLine("Не целое число");
+int? k=0;
+if (Inttest(s)==true)
+ {
+    for(int i=0;i<s.Length;i++)
+      if (char.IsDigit(s[i])) 
+           k+= int.Parse($"{s[i]}");
+               System.Console.WriteLine(k);
+ }
+ else System.Console.WriteLine("Введено не корректное число");
