83230074a88ae705fc2fe8567f9f7535b0e3f008
Dan <baitursonov@gmail.com>
2022-12-30 23:11:32 +0600

    Home work 9

--- 93/Program.cs
+++ 93/Program.cs
@@ -1,6 +1,5 @@
 ﻿//93. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
-//69. Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
 int n=6;
 int m=5;
 int[,] Random2DArray(int n,int m,int min=1,int max=5)
@@ -23,24 +22,20 @@ void Print2DArray(int[,] a)
         }
 }
 
-void LineSort(int[,] a,int[,] b)
-{
+void ArrSum(int[,] a,int[,] b,int[,] c)
+{    
      for(int i=0;i<a.GetLength(0);i++)    
         for(int k=0;k<a.GetLength(1);k++)
             for(int j=0;j<a.GetLength(1)-1;j++)
-                if (a[i,j]>a[i,j+1])
-                {
-                    int t=a[i,j];
-                    a[i,j]=a[i,j+1];
-                    a[i,j+1]=t;                    
-                }
+                c[i,j] = a[i,j]*b[i,j];
 }
 
 int[,] a=Random2DArray(n,m);
 int[,] b=Random2DArray(n,m);
-int[,] c;
+int[,] c=Random2DArray(n,m);
 Print2DArray(a);
 System.Console.WriteLine();
-//LineSort(c);
 Print2DArray(b);
-//LineSort(a);
+System.Console.WriteLine();
+ArrSum(a,b,c);
+Print2DArray(c);
